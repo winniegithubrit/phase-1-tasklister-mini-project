f568ce61f3ba9c104b1112e6d9e634ccf7b09021
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-03-25 22:54:01 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
+/*document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  (document.querySelector('form')).addEventListener('submit', (e)=>{
+    e.preventDefault();
+    handleTodo(e);
+  })
+});*/
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const taskList = new TaskList();
+  
+
+  
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  
+  const taskUl = document.getElementById("tasks");
+
+  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskList.createNewTask(newTaskDescription.value);
+    
+    e.target.reset();
+    renderApp();
+  });
+
+  taskUl.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp();
+    }
+  });
 });
